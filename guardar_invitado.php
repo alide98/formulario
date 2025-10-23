7f7387d737dba34f2306f2fef1b89113960bbd9e
alide98 <johanalirioo@gmail.com>
2025-10-23 14:29:26 -0500

    ajustes

--- guardar_invitado.php
+++ guardar_invitado.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = ""; // si tu instalación tiene password, cámbialo aquí
+$password = ""; 
 $dbname = "proyecto";
 
 // Crear conexión
